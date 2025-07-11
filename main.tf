da5af41a65bb65f371c794ffca6870844b056252
Vikash Kumar <Kvikash668@gmail.com>
2025-07-12 01:08:55 +0530

    first main.tf

---
+++ main.tf
@@ -0,0 +1,55 @@
+/*
+terraform {
+  cloud {
+
+    organization = "TERRAFORM_LEARN668"
+
+    workspaces {
+      name = "LEARN668_CLI"
+    }
+  }
+}
+*/
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-west-2"
+}
+
+# Create a VPC
+resource "aws_vpc" "peer" {
+  cidr_block = "10.1.0.0/16"
+}
+/*
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+resource "aws_instance" "main" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = "myfirstvm"
+  }
+}
+*/
